cc4c2cc7d5a37803a266d3c6ab59f087c94bd147
maciej-ol <maciekk.oldakowskii@gmail.com>
2024-03-16 14:48:20 +0100

    Update task 1 image input type

--- task_1_modelstealing/endpoints/model_stealing.py
+++ task_1_modelstealing/endpoints/model_stealing.py
@@ -1,16 +1,21 @@
-def model_stealing(path_to_png_file: str):
+from PIL import Image
+import requests
+from PIL import Image
+import io
+
+def model_stealing(image: Image):
     SERVER_URL = "[paste server url here]"
     ENDPOINT = "/modelstealing"
     URL = SERVER_URL + ENDPOINT
 
     TEAM_TOKEN = "[paste your team token here]"
 
-    with open(path_to_png_file, "rb") as img_file:
-        response = requests.get(
-            URL, files={"file": img_file}, headers={"token": TEAM_TOKEN}
-        )
 
-        if response.status_code == 200:
-            return response.content["representation"]
-        else:
-            raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
+    response = requests.get(
+        URL, files={"file": io.BufferedReader(image.tobytes())}, headers={"token": TEAM_TOKEN}
+    )
+
+    if response.status_code == 200:
+        return response.content["representation"]
+    else:
+        raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
