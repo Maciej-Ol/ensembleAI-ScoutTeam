ebbe197f81c9ac3e5a9a5cc75c7ddad007f10518
OhDuck47 <kp.oldakowsk@student.uw.edu.pl>
2024-03-16 13:14:59 +0100

    create new files

--- task_2_sybilattack/load_dataset.py
+++ task_2_sybilattack/load_dataset.py
@@ -3,6 +3,6 @@ from taskdataset import TaskDataset
 
 
 if __name__ == "__main__":
-    dataset = torch.load("sybilattack/data/ExampleSybilAttack.pt")
+    dataset = torch.load("task_2_sybilattack/data/ExampleSybilAttack.pt")
 
     print(dataset.ids, dataset.imgs, dataset.labels)
