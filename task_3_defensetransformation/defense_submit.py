b5f410d1a067e0387bd01d39d1692428ba9060d3
maciej-ol <maciekk.oldakowskii@gmail.com>
2024-03-17 01:28:27 +0100

    add exception handling

--- task_3_defensetransformation/defense_submit.py
+++ task_3_defensetransformation/defense_submit.py
@@ -9,14 +9,17 @@ def defense_submit(path_to_npz_file: str):
     endpoint = "/defense/submit"
     url = SERVER_URL + endpoint
     with open(path_to_npz_file, "rb") as f:
-        response = requests.post(url, files={"file": f}, headers={"token": TEAM_TOKEN})
-        if response.status_code == 200:
-            print("Request ok")
-            print(response.json())
-            answer = f"Request ok, response: {response.json()}"
-        else:
-            answer = f"Defense submit failed. Code: {response.status_code}, content: {response.json()}"
-    return response.json()
+        try:
+            response = requests.post(url, files={"file": f}, headers={"token": TEAM_TOKEN})
+            if response.status_code == 200:
+                print("Request ok")
+                print(response.json())
+                answer = f"Request ok, response: {response.json()}"
+            else:
+                answer = f"Defense submit failed. Code: {response.status_code}, content: {response.json()}"
+        except Exception as e:
+            answer = f"Server down. Exception: {e}"
+    return answer
 
 
 def apply_defense_transformation_3(X,padding_size=100,st=0.3):
@@ -32,15 +35,15 @@ def apply_defense_transformation_3(X,padding_size=100,st=0.3):
     X = np.hstack((X, padding))
     return X
 
-
-
-def save_file(st):
+def transform_file(st):
     data_sub = np.load("task_3_defensetransformation/data/DefenseTransformationSubmit.npz")
     rep = apply_defense_transformation_3(data_sub["representations"],st=st)
 
     # Convert from float64 to float32                                
     rep = rep.astype(np.float32)
-                                    
+    return rep
+
+def save_file(rep):           
     # Apply defense transformation (e.g., your transformation)
     np.savez(
         "task_3_defensetransformation/data/example_submission.npz",
@@ -52,7 +55,7 @@ def save_response(response, i):
     output_file = "task_3_defensetransformation/data/output.txt"  # Name of the output text file
     with open(output_file, mode='a') as file:
         file.write(response + "\n")
-        file.write("Step: "+i + "\n")
+        file.write(f"Step: {i}" + "\n")
 
 
 
@@ -61,11 +64,13 @@ if __name__ == "__main__":
     list = [0.1, 0.2, 0.05, 0.15, 0.3, 0.4, 0.5]
 
     for i in list:
-        save_file(i)
+        rep = transform_file(i)
+        save_file(rep)
         respone = defense_submit("task_3_defensetransformation/data/example_submission.npz")
-        save_response(respone, i)
         print("I have finished defense_submit successfully!")
-        time.sleep(3660)  # Sleep for 4000 seconds (about 1 hour)
+        save_response(respone, i)
+        print("Response saved!")
+        time.sleep(3660)  # Sleep for about 1 hour
 
 
 
